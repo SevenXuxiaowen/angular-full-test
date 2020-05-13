a915440d578285357e80523d31d8a8fc84d76b7d
SevenXuxiaowen <xiaowen.xu.china@gmail.com>
2020-05-12 22:43:20 -0700

    initialize app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MLModule } from './ml.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { StudyComponent } from './study/study.component';
+import { CurrentStudyComponent } from './study/current-study/current-study.component';
+import { NewStudyComponent } from './study/new-study/new-study.component';
+import { PastStudyComponent } from './study/past-study/past-study.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    StudyComponent,
+    CurrentStudyComponent,
+    NewStudyComponent,
+    PastStudyComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
